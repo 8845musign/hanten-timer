eafee16f8af94c5a70b0fb6b69d5e05f64aee354
8845musign <8845musign@gmail.com>
2017-05-28 15:44:04 +0900

    Add dev tool setting

--- pages/indexStore.js
+++ pages/indexStore.js
@@ -1,8 +1,18 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import TimerReducer from '../redux/modules/Timer'
 
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose
+
+
+const enhancer = composeEnhancers(
+  applyMiddleware()
+)
+
 export const indexStore = (initialState = {}) => {
-  return createStore(TimerReducer, initialState, applyMiddleware())
+  return createStore(TimerReducer, initialState, enhancer)
 }
 
 export default indexStore
