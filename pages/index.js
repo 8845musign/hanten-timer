ca78f14007e8521a5dbc15459d84d352714b64db
8845musign <8845musign@gmail.com>
2017-08-18 01:53:47 +0900

    Add bluma css.

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Head from 'next/head'
 import withRedux from 'next-redux-wrapper'
 
 import indexStore from './indexStore'
@@ -8,7 +9,15 @@ import TimerContainer from '../components/timer/container'
 class Index extends React.Component {
   render () {
     return (
-      <TimerContainer />
+      <div>
+        <Head>
+          <title>My page title</title>
+          <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+          <link rel='stylesheet' type='text/css' href='/static/css/bluma.css' />
+        </Head>
+
+        <TimerContainer />
+      </div>
     )
   }
 }
