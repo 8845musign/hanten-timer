f43f09a8a027bc5ea3deaa4d9f57b8742532f1c4
8845musign <8845musign@gmail.com>
2017-08-19 21:12:11 +0900

    Styling timer page.

--- components/timer/index.js
+++ components/timer/index.js
@@ -48,34 +48,42 @@ type Props = {
 
 export default ({ startTimer, stopTimer, pauseTimer, elapsedTime, settingTime, setTime, isStart, isPause, taskTitle, changeTaskTitle }: Props) => {
   return (
-    <div>
-      <style global jsx>{`
-        body {
-          background-color: #FF5252
-        }
-      `}</style>
-      <fieldset className='field'>
-        <label className='label'>Task Title</label>
-        <div className='control'>
-          <input className='input' type='text' value={taskTitle} onChange={onChangeTitle(changeTaskTitle)} />
+    <section className='section'>
+      <div className='container'>
+        <div className='tile notification'>
+          <label className='label'>Task Title</label>
+          <div className='control'>
+            <input className='input' type='text' value={taskTitle} onChange={onChangeTitle(changeTaskTitle)} />
+          </div>
         </div>
-      </fieldset>
 
-      <div>
-        Time Reaming : {TimeUtil.unix2mmss(convertToTimeRemaing(settingTime, elapsedTime))} sec
+        <div className='tile notification is-info'>
+          Time Reaming : {TimeUtil.unix2mmss(convertToTimeRemaing(settingTime, elapsedTime))} sec
+          <progress className='progress' value={elapsedTime} max={settingTime} >15%</progress>
+        </div>
+
+        <div className='tile notification'>
+          <div className='columns'>
+            <div className='column'>
+              <button className='button is-info is-outlined' onClick={onClickStart(startTimer)}>startTimer</button>
+            </div>
+
+            <div className='column'>
+              <button className='button' onClick={pauseTimer} disabled={isPause || !isStart}>Pause</button>
+            </div>
+
+            <div className='column'>
+              <button className='button is-danger is-outlined' onClick={onClickStop(stopTimer)}>stopTimer</button>
+            </div>
+          </div>
+        </div>
+
+        <div className='tile notification'>
+          <input className='input' value={settingTime} onChange={onChange(setTime)} readOnly={isStart === true} />
+        </div>
+
+        <Link href='/task'><a>> tasks</a></Link>
       </div>
-
-      <fieldset>
-        <button onClick={onClickStart(startTimer)}>startTimer</button>
-        <button onClick={pauseTimer} disabled={isPause || !isStart}>Pause</button>
-        <button onClick={onClickStop(stopTimer)}>stopTimer</button>
-      </fieldset>
-
-      <fieldset>
-        <input value={settingTime} onChange={onChange(setTime)} readOnly={isStart === true} />
-      </fieldset>
-
-      <Link href='/task'><a>tasks</a></Link>
-    </div>
+    </section>
   )
 }
